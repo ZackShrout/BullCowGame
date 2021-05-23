ad7241469bdade85bd22c3e46dac5866787e0792
Zack Shrout <zshrout@gmail.com>
2021-05-22 20:07:45 -0500

    Implemented basic game loop, game over functionality, and word length checking.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,55 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    PrintLine("Welcome to Bull Cow!");
-    PrintLine("Please input something.");
-    PrintLine("Then press ENTER...");
+    InitGame();
+    
+    //PrintLine(TEXT("The HiddenWord is %s"), *HiddenWord);
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
+    if (bGameOver)
+    {
+        ClearScreen();
+        InitGame();
+    }
+    else
+    {
+        if (Input.Len() == HiddenWord.Len())
+        {
+            if (Input == HiddenWord)
+            {
+                PrintLine(TEXT("Correct!"));
+                EndGame();
+            }
+            else
+            {
+                PrintLine(TEXT("Incorrect."));
+                EndGame();
+            }
+        }
+        else
+        {
+            PrintLine(TEXT("I told you to guess a %i letter word... \ncan't you count?"), HiddenWord.Len());
+        }
+    }
+
+    
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to Bull Cow!"));
+    PrintLine(TEXT("Please guess the %i letter word."), HiddenWord.Len());
+    PrintLine(TEXT("You have %i guesses..."), Lives);
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press ENTER to play again..."));
 }
