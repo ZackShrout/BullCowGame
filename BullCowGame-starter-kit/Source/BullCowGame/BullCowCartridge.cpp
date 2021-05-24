eab2e9a1354e18934c97641f06fb6f70071722f8
Zack Shrout <zshrout@gmail.com>
2021-05-24 07:07:03 -0500

    Refactored to get rid of nested checks, and added an isogram check.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -12,39 +12,54 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ProcessGuess(Input);
+}
+
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
+{
     if (bGameOver)
     {
         ClearScreen();
         InitGame();
-    }
-    else
-    {
-        if (Input.Len() == HiddenWord.Len())
-        {
-            if (Input == HiddenWord)
-            {
-                PrintLine(TEXT("Correct!"));
-                EndGame();
-            }
-            else
-            {
-                PrintLine(TEXT("Incorrect."));
-                EndGame();
-            }
-        }
-        else
-        {
-            PrintLine(TEXT("I told you to guess a %i letter word... \ncan't you count?"), HiddenWord.Len());
-        }
+        return;
     }
 
-    
+    if (Guess == HiddenWord)
+    {
+        ClearScreen();
+        PrintLine(TEXT("Correct! You have won!"));
+        EndGame();
+        return;
+    }
+
+    if (Guess.Len() != HiddenWord.Len())
+    {
+        PrintLine(TEXT("I told you to guess a %i letter word... \ncan't you count?"), HiddenWord.Len());
+        return;
+    }
+
+    if (!IsIsogram(Guess))
+    {
+        PrintLine(TEXT("There can be no repeating letters."));
+        return;
+    }
+
+    if (--Lives > 0)
+    {
+        PrintLine(TEXT("Incorrect. You have %i more guesses."), Lives);
+        return;
+    }
+
+    ClearScreen();
+    PrintLine(TEXT("Incorrect. You have lost."));
+    PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+    EndGame();
 }
 
 void UBullCowCartridge::InitGame()
 {
     HiddenWord = TEXT("cake");
-    Lives = 4;
+    Lives = HiddenWord.Len();
     bGameOver = false;
 
     PrintLine(TEXT("Welcome to Bull Cow!"));
@@ -52,8 +67,25 @@ void UBullCowCartridge::InitGame()
     PrintLine(TEXT("You have %i guesses..."), Lives);
 }
 
+bool UBullCowCartridge::IsIsogram(const FString& Word) const
+{
+    TCHAR letter;
+
+    for (int32 i = 0; i < Word.Len(); i++)
+    {
+        letter = Word[i];
+        for (int32 j = 0; j < Word.Len(); j++)
+        {
+            if (j == i) continue;
+            if (Word[j] == letter) return false;
+        }
+    }
+    return true;
+}
+
 void UBullCowCartridge::EndGame()
 {
     bGameOver = true;
     PrintLine(TEXT("Press ENTER to play again..."));
 }
+
